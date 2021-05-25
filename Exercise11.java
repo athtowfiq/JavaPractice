543201f0c0393b99774a191bc98bdb5afbe5c65a
Towfiq <you@example.com>
2021-05-25 20:17:29 +0600

    exercise of 23 may

---
+++ Exercise11.java
@@ -0,0 +1,12 @@
+public class Exercise11 {
+    public static void main(String[] args) {
+        double r= 7.5;
+        final double pi = 3.1416;
+        double perimeter= 2*pi*r;
+        double area=pi*r*r;
+        System.out.println("Perimeter is= " +perimeter);
+        System.out.println("Area is= " +area);
+
+
+    }
+}
---
+++ Exercise5.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int firstNumber =input.nextInt();
+        int secondNumber = input.nextInt();
+        int product=firstNumber*secondNumber;
+        System.out.println(product);
+
+    }
+}
